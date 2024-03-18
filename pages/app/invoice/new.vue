4257abab757a64806b630ad07e8d08b236811008
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-03-18 22:51:23 +0530

    Revert "Invoice 30%"
    This reverts commit 43939b6059390494b34b06a0452096b6b2061c6b.

--- pages/app/invoice/new.vue
+++ pages/app/invoice/new.vue
@@ -1,27 +1,41 @@
 <script setup>
-const items = [
-  [{
-    label: 'Profile',
-    avatar: {
-      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-    }
-  }]
-]
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(201, 203, 207, 0.2)",
+      ],
+      borderColor: [
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(153, 102, 255)",
+        "rgb(201, 203, 207)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 </script>
 
 <template>
   <div>
     <NuxtLayout name="app">
       <div
-        class="w-full flex flex-col bg-secondary-bkg dark:border dark:border-slate-700 rounded-xl shadow dark:shadow-black p-4">
-        <h1 class="md:text-lg sm:text-lg mb-4">Create Invoice</h1>
-        <div class="flex flex">
-          <UDropdown :items="items" mode="hover" :popper="{ placement: 'bottom-start' }">
-            <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
-          </UDropdown>
-          <UInput color="gray" variant="outline" placeholder="Search..." />
-          <UInput color="gray" variant="outline" placeholder="Search..." />
-        </div>
+        class="flex flex-col bg-secondary-bkg rounded-xl shadow dark:shadow-black p-8"
+      >
+        <UChart type="bar" id="myChart" :data="data" />
       </div>
     </NuxtLayout>
   </div>

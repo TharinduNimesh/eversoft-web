43939b6059390494b34b06a0452096b6b2061c6b
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-03-18 19:25:16 +0530

    Invoice 30%

--- components/App/SideBar/Item.vue
+++ components/App/SideBar/Item.vue
@@ -1,90 +1,43 @@
-<script setup lang="ts">
-import type { RouteOptions } from "~/datatypes";
-
-const props = defineProps({
-  options: {
-    type: Object as () => RouteOptions,
+<script setup>
+defineProps({
+  name: {
+    type: String,
     required: true,
   },
+  icon: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  subRoutes: {
+    type: Array,
+    default: null,
+  },
 });
-const router = useRouter();
-const sidebarStore = useSidebarStore();
-const isActive = computed(() => {
-  return router.currentRoute.value.path.includes(props.options.path);
-});
-
-function takeAction() {
-  if (props.options.subRoutes) {
-    if (!sidebarStore.isOpen) {
-      sidebarStore.toggle();
-    }
-    sidebarStore.setOpenedSubRoute(props.options.path);
-  } else {
-    router.push(props.options.path);
-  }
-}
 </script>
 
 <template>
   <div class="w-full flex flex-col">
-    <div
-      class="w-full flex cursor-pointer"
-      :class="{
-        'bg-primary/10': isActive,
-        'hover:bg-gray-100 dark:hover:bg-gray-700/20': !isActive
-      }"
-      @click="takeAction"
-    >
-      <span
-        class="w-1"
-        :class="{
-          'bg-primary': isActive,
-        }"
-      ></span>
-      <div
-        class="relative flex-1 flex px-5 py-4 items-center gap-3"
-        :class="{
-          'md:justify-center': !sidebarStore.isOpen,
-        }"
-      >
-        <Icon
-          :name="options.icon"
-          class="text-2xl"
-          :class="{
-            'md:text-3xl': !sidebarStore.isOpen,
-          }"
-        />
-        <span
-          class="text font-bold uppercase"
-          :class="{
-            'md:hidden': !sidebarStore.isOpen,
-          }"
-          >{{ options.name }}</span
-        >
-        <Icon
-          v-show="options.subRoutes"
-          name="ic:round-keyboard-arrow-down"
-          class="absolute text-2xl right-3 transition-transform duration-300"
-          :class="{
-            'md:!hidden': !sidebarStore.isOpen,
-            '-rotate-90': !(sidebarStore.openedSubRoute == props.options.path),
-          }"
-        />
+    <div class="w-full flex cursor-pointer" :class="{
+      'bg-primary/10': isActive,
+    }">
+      <span class="w-1" :class="{
+      'bg-primary': isActive,
+    }"></span>
+      <div class="relative flex-1 flex px-5 py-3 items-center gap-3">
+        <Icon :name="icon" class="text-2xl" />
+        <span class="text font-bold uppercase">{{ name }}</span>
+        <Icon v-if="subRoutes" name="ic:round-keyboard-arrow-down" class="absolute text-2xl right-3" />
       </div>
     </div>
-    <div
-      class="w-full pl-14 text-gray-500 transition-[max-height] duration-300 overflow-hidden"
-      v-show="options.subRoutes"
-      :class="{
-        'md:hidden': !sidebarStore.isOpen,
-        'max-h-0': !(sidebarStore.openedSubRoute == props.options.path),
-      }"
-    >
-      <AppSideBarSubItem
-        v-for="subRoute in options.subRoutes"
-        :key="subRoute.path"
-        :options="subRoute"
-      />
+    <div class="w-full pl-14 text-gray-500" v-if="subRoutes">
+      <div class="w-full py-3 flex items-center gap-4">
+        <Icon name="bxs:cricket-ball" class="text-sm" />
+        <span class="text-sm font-semibold uppercase">Add New Post</span>
+      </div>
     </div>
   </div>
 </template>
